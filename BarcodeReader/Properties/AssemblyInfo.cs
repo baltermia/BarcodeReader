99444b09dc4dd79b3306da30ac6d9a8c22010663
speyck <54800864+speyck@users.noreply.github.com>
2021-08-04 14:31:54 +0200

    add nuspec file and edited assemblyinfo for release

--- BarcodeReader/Properties/AssemblyInfo.cs
+++ BarcodeReader/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BarcodeReader")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("BarcodeReader is a facade of the ZXing.Net library and allows you to decode images containing Barcodes.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BarcodeReader")]
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
